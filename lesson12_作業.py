59081d8cadf981c2c4391ec0cae6451954f7c5b3
Sf-Git-2023 <sf.chang232tpv@gmail.com>
2023-10-01 23:15:07 +0800

    update 1001-2315

--- lesson12_作業.py
+++ lesson12_作業.py
@@ -14,6 +14,11 @@ class Player:
             dice2 = random.randint(1,6)
             dice3 = random.randint(1,6)
             dice4 = random.randint(1,6)
+            dice1 = 1
+            dice2 = 1
+            dice3 = 1
+            dice4 = 1
+
             D = [dice1, dice2, dice3, dice4]
             for x in range(3):
                 for j in range(x+1,4) :
@@ -28,7 +33,7 @@ class Player:
                 score = D[0] + 12
             else :
                 score = 0        
-        return f'骰子一:{dice1} 骰子二:{dice2} 骰子三:{dice3} 骰子四:{dice4}\n{D}\n得分:{score}'
+        return f'骰子1:{dice1} 骰子2:{dice2} 骰子3:{dice3} 骰子4:{dice4}\n{D}\n得分:{score}'
 
 #property
     @property
@@ -37,15 +42,15 @@ class Player:
 
 #被呼叫時傳出字串
     def __repr__(self) -> str:
-        descript = f"姓名:{self.name}"
-        return descript
+        PlayerName = f"姓名:{self.name}"
+        return PlayerName
 
-p1 = Player('甲')
+p1 = Player('玩家1')
 print(p1)
 print(p1.value)
 
 print()
 
-p2 = Player('乙')
+p2 = Player('玩家2')
 print(p2)
 print(p2.value)
