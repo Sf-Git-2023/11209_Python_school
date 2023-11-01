155d1d1645c6a980347d1bbd3297da958518ee36
Sf-Git-2023 <sf.chang232tpv@gmail.com>
2023-11-01 10:27:30 +0800

    update 1101-1027

---
+++ youbike1101/index.py
@@ -0,0 +1,35 @@
+import tkinter as tk
+from tkinter import ttk
+from tkinter import messagebox
+from threading import Timer
+import datasource
+
+class Window(tk.Tk):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        try:
+            datasource.updata_sqlite_data()
+        except Exception:
+            messagebox.showerror("錯誤",'網路不正常\n將關閉應用程式\n請稍後再試')
+            self.destroy()
+
+     
+
+def main(): 
+    
+
+    
+    def update_data(w:Window)->None:
+        datasource.updata_sqlite_data()
+        window.after(60*1000,update_data,w)
+          
+
+    window = Window()
+    window.title('台北市youbike2.0')
+    window.geometry('600x300')
+    window.resizable(width=False,height=False)
+    update_data(window)          
+    window.mainloop()
+
+if __name__ == '__main__':
+    main()
