b849dcf101bd14922e1cb2d9861a5603e96ef349
Sf-Git-2023 <sf.chang232tpv@gmail.com>
2023-11-07 12:45:45 +0800

    update 1107-12:45

--- 1102_作業/index.py
+++ 1102_作業/index.py
@@ -12,7 +12,7 @@ class Window(tk.Tk):
                                           corner_radius=0,
                                           )
         map_widget.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
-        marker_1 = map_widget.set_position(25.038128318756307, 121.56306490172479,marker=True) #台北市位置
+        marker_1 = map_widget.set_position(25.043199841649802, 121.57041045419318,marker=True) #台北市位置
         map_widget.set_zoom(15) #設定顯示大小
         marker_1.set_text("台北市中心")
         # marker_1.set_position(48.860381, 2.338594)  # 改變位置
