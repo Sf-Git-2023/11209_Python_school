1a17ebff4e5fe6467f42d607f0fbc98d47e0b2f9
Sf-Git-2023 <sf.chang232tpv@gmail.com>
2023-12-22 11:36:24 +0800

    up 1222-1136

--- flask_dash/main.py
+++ flask_dash/main.py
@@ -3,6 +3,7 @@ from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from werkzeug.serving import run_simple
 from dash_file.dash_app1 import dash1
 from dash_file.dash_app2 import dash2
+from dash_file.dash_app3 import dash3
 from auth.auth import blueprint_auth
 import secrets
 
@@ -13,12 +14,17 @@ app.config['SECRET_KEY'] = secrets.token_hex(16)
 application = DispatcherMiddleware(
     app,
     {"/dash/app1": dash1.server,
-     "/dash/app2": dash2.server}
+     "/dash/app2": dash2.server,
+     "/dash/app3": dash3.server,}
 )
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/car")
+def car():
+    return render_template("car.html")
 
 if __name__ == "__main__":
     run_simple("localhost", 8080, application,use_debugger=True,use_reloader=True)
